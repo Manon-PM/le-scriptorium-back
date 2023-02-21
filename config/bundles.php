3267532e5431743e07b11805696f7d6f34976271
Julien-Rousset <noxtrad77@gmail.com>
2023-02-21 14:21:25 +0100

    add nelmio security bundle and nelmio configuration to api path

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\SecurityBundle\NelmioSecurityBundle::class => ['all' => true],
 ];
