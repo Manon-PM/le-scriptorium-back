9aadfe67b310ab8e144011c1f5f1ff8a76c8ff89
Julien-Rousset <noxtrad77@gmail.com>
2023-02-28 12:43:38 +0100

    remove use serialiser to pdf generator

--- src/Controller/Api/PdfController.php
+++ src/Controller/Api/PdfController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
