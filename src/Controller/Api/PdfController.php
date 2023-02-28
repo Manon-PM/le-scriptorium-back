287dcec7be9f90d78f897fb88bcab58e00a01489
Julien-Rousset <noxtrad77@gmail.com>
2023-02-28 12:42:07 +0100

    add checker to pdf generator

--- src/Controller/Api/PdfController.php
+++ src/Controller/Api/PdfController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Entity\Sheet;
+use App\Utils\CheckSerializer;
 use App\Utils\PdfService;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,7 +19,7 @@ class PdfController extends AbstractController
     /**
      * @Route("/api/generator", name="app_api_pdf")
      */
-    public function generatePdf(PdfService $pdf, SerializerInterface $serializer, Request $request): JsonResponse
+    public function generatePdf(PdfService $pdf, CheckSerializer $checker, Request $request): JsonResponse
     {
         // On instencie FilesystemAdapter pour gérer le cache
         $cache = new FilesystemAdapter();
@@ -39,11 +40,19 @@ class PdfController extends AbstractController
         });
 
         // On deserialize $jsonContent (Request) pour l'utiliser dans notre template twig
-        $jsonContent = $serializer->deserialize($jsonContent, Sheet::class, 'json');
+        $result = $checker->serializeValidation($jsonContent, Sheet::class);
+
+        if (!$result instanceof Sheet) {
+            return $this->json(
+                ["error" => $result],
+                404,
+                []
+            );
+        }
 
         //On stock le template twig avec le contenu de jsonContent (Request) dans $html
         $html = $this->render('api/pdf/fiche.html.twig', [
-            'pdfContent' => $jsonContent,
+            'pdfContent' => $result,
         ]);
 
         // On envoie le template twig à la methode de DomPdf dans le pdfService
